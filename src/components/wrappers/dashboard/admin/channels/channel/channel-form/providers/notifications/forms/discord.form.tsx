ee049d81154d47e3057d467ebf1f41e4d612a326
Théo LAGACHE <theo.lagache@soluce-technologies.com>
2026-03-04 13:21:19 +0100

    add: password input on sensible fields

--- src/components/wrappers/dashboard/admin/channels/channel/channel-form/providers/notifications/forms/discord.form.tsx
+++ src/components/wrappers/dashboard/admin/channels/channel/channel-form/providers/notifications/forms/discord.form.tsx
@@ -1,30 +1,38 @@
-import {UseFormReturn} from "react-hook-form";
-import {FormControl, FormField, FormItem, FormLabel, FormMessage} from "@/components/ui/form";
-import {Input} from "@/components/ui/input";
-import {Separator} from "@/components/ui/separator";
-
+import { UseFormReturn } from "react-hook-form";
+import {
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Separator } from "@/components/ui/separator";
+import { PasswordInput } from "@/components/ui/password-input";
 
 type NotifierDiscordFormProps = {
-    form: UseFormReturn<any, any, any>
-}
+  form: UseFormReturn<any, any, any>;
+};
 
-export const NotifierDiscordForm = ({form}: NotifierDiscordFormProps) => {
-    return (
-        <>
-            <Separator className="my-1"/>
-            <FormField
-                control={form.control}
-                name="config.discordWebhook"
-                render={({field}) => (
-                    <FormItem>
-                        <FormLabel>Discord Webhook URL</FormLabel>
-                        <FormControl>
-                            <Input {...field} placeholder="https://discord.com/api/webhooks/..."/>
-                        </FormControl>
-                        <FormMessage/>
-                    </FormItem>
-                )}
-            />
-        </>
-    )
-}
+export const NotifierDiscordForm = ({ form }: NotifierDiscordFormProps) => {
+  return (
+    <>
+      <Separator className="my-1" />
+      <FormField
+        control={form.control}
+        name="config.discordWebhook"
+        render={({ field }) => (
+          <FormItem>
+            <FormLabel>Discord Webhook URL</FormLabel>
+            <FormControl>
+              <PasswordInput
+                {...field}
+                placeholder="https://discord.com/api/webhooks/..."
+              />
+            </FormControl>
+            <FormMessage />
+          </FormItem>
+        )}
+      />
+    </>
+  );
+};
