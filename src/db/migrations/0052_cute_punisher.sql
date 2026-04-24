f40c27629379ff69531be9a447f52c0dce9e53ec
Charles GTE <charlesgte31@gmail.com>
2026-04-24 21:34:42 +0200

    fix: migration error on 2fa

--- src/db/migrations/0052_cute_punisher.sql
+++ src/db/migrations/0052_cute_punisher.sql
@@ -1,1 +1,4 @@
-ALTER TABLE "two_factor" ADD COLUMN "verified" boolean NOT NULL;
+ALTER TABLE "two_factor" ADD COLUMN IF NOT EXISTS "verified" boolean DEFAULT false;
+UPDATE "two_factor" SET "verified" = true WHERE "verified" = false;
+ALTER TABLE "two_factor" ALTER COLUMN "verified" SET NOT NULL;
+ALTER TABLE "two_factor" ALTER COLUMN "verified" DROP DEFAULT;
