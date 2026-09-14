68c3ebf938a607024eae794cc3374c30bb12a536
Théo LAGACHE <theo.lagache@soluce-technologies.com>
2026-09-14 10:44:03 +0200

    change cli cmd

--- src/features/agents/components/agent-key-modal.tsx
+++ src/features/agents/components/agent-key-modal.tsx
@@ -15,7 +15,7 @@ export type agentRegistrationDialogProps = PropsWithChildren<{
 
 export function AgentModalKey(props: agentRegistrationDialogProps) {
     const edge_key = generateEdgeKey(props.agent.overrideUrl ?? getServerUrl(), props.agent.id);
-    const command = `portabase agent "${props.agent.name}" --key ${edge_key}`;
+    const command = `portabase agent create"${props.agent.name}" --key ${edge_key}`;
 
     return (
         <Dialog>
